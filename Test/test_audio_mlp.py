0266d2574116473813ac2df709a79d39b26a4d83
Pavi Ponnolu <156141576+pponn@users.noreply.github.com>
2025-06-20 14:07:43 -0400

    Add code comments to test_audio_mlp.py

--- Test/test_audio_mlp.py
+++ Test/test_audio_mlp.py
@@ -12,22 +12,26 @@ import joblib
 from Models.mlp_model import predict as predict_mlp
 
 def main(duration=3, sr=48000):
+    #1. Record audio
     print(f"Recording {duration} seconds of audio... Speak now!")
     recording = sd.rec(int(duration * sr), samplerate=sr, channels=1, dtype='float32')
     sd.wait()
     recording = np.squeeze(recording)
 
+    #2. Save to temp file
     with tempfile.NamedTemporaryFile(suffix=".wav", delete=False) as tmp:
         sf.write(tmp.name, recording, sr)
         tmp_path = tmp.name
 
     print(f"Saved temporary audio to: {tmp_path}")
 
+    #3. Predict with MLP model
     pred_idx = predict_mlp(tmp_path)
 
+    #4. Load label encoder and predict emotion
     label_encoder_path = os.path.join("Trained_Models", "label_encoder.pkl")
     label_encode = joblib.load(label_encoder_path)
-    predicted_emotion = label_encode.inverse_transform([pred_idx])[0]
+    predicted_emotion = label_encode.inverse_transform([pred_idx])[0] #show the predicted emotion for the given audio from the mlp model
 
     print(f" Predicted Emotion: {predicted_emotion}")
 
