0eba41a835215987a3029317bdda4691a821ffb2
Josh-Len <165314862+Josh-Len@users.noreply.github.com>
2025-06-22 01:34:15 -0400

    Update knn_model.py to address the hugging face token issue

--- Models/knn_model.py
+++ Models/knn_model.py
@@ -11,18 +11,17 @@ HF_REPO_ID = "shreyasshukla/emotion-cnn-single"
 HF_FILENAME = "knn.pkl"
 
 @functools.lru_cache(maxsize=1)
-def load_knn_model(model_path: str = None):
-    if model_path is None or not os.path.exists(model_path):
-        model_path = hf_hub_download(
-            repo_id=HF_REPO_ID,
-            filename=HF_FILENAME,
-            repo_type="model",
-            token=True
-        )
+def load_knn_model():
+    model_path = hf_hub_download(
+        repo_id=HF_REPO_ID,
+        filename=HF_FILENAME,
+        repo_type="model",
+        token=os.environ.get("HUGGINGFACE_HUB_TOKEN")
+    )
     return joblib.load(model_path)
 
 def predict(audio_path: str, model_path: str = None) -> int:
-    knn = load_knn_model(model_path)
+    knn = load_knn_model()
     spec = extract_spectrogram_from_path(
         audio_path,
         sr=48000,
